22b60d8ed21dbe7b0146228427eaea7df5cb206f
Nic <nicolecadman@icloud.com>
2023-05-01 16:02:27 +0000

    amended link to treatments

--- treatments/models.py
+++ treatments/models.py
@@ -13,18 +13,7 @@ class Client(db.Model):
     name = db.Column(db.String(50), nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
     phone = db.Column(db.String(20), nullable=False)
-    lashes = db.relationship(
-        "Lash",
-        backref="client",
-        cascade="all, delete",
-        lazy=True
-    )
-    brows = db.relationship(
-        "Brow",
-        backref="client",
-        cascade="all, delete",
-        lazy=True
-    )
+    treatments = db.relationship('Treatment', backref='client', lazy=True)
 
     def __repr__(self):
         # __repr__ to represent itself in the form of a string
