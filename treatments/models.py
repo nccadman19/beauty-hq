eb46c237185281af8ebb678ba5d320fcbe233588
Nic <nicolecadman@icloud.com>
2023-05-06 12:59:19 +0000

    Incorporated treatment into the add client section

--- treatments/models.py
+++ treatments/models.py
@@ -13,11 +13,34 @@ class Client(db.Model):
     name = db.Column(db.String(50), nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
     phone = db.Column(db.String(20), nullable=False)
-    treatments = db.relationship('Treatment', backref='client', lazy=True)
+    lash_type = db.Column(db.String(20), nullable=True)
+    lash_notes = db.Column(db.Text, nullable=True)
+    brow_type = db.Column(db.String(20), nullable=True)
+    brow_notes = db.Column(db.Text, nullable=True)
 
     def __repr__(self):
         # __repr__ to represent itself in the form of a string
         return self.name
+
+    @property
+    def formatted_lash_type(self):
+        if self.lash_type == 'classic':
+            return 'Classic'
+        elif self.lash_type == 'hybrid':
+            return 'Hybrid'
+        elif self.lash_type == 'russian':
+            return 'Russian'
+        else:
+            return 'N/A'
+
+    @property
+    def formatted_brow_type(self):
+        if self.brow_type == 'brow_tint':
+            return 'Brow & Tint'
+        elif self.brow_type == 'lamination':
+            return 'Lamination'
+        else:
+            return 'N/A'
 
 
 class User(UserMixin, db.Model):
@@ -34,57 +57,3 @@ class User(UserMixin, db.Model):
     @staticmethod
     def get_by_id(id):
         return User.query.get(int(id))
-
-
-class Treatment(db.Model):
-    TREATMENT_TYPES = [
-        ('L', 'Lashes'),
-        ('B', 'Brows'),
-        ('BL', 'Both'),
-    ]
-
-    class Lash(db.Model):
-        id = db.Column(db.Integer, primary_key=True)
-        client_id = db.Column(
-            db.Integer,
-            db.ForeignKey('client.id'),
-            nullable=False
-            )
-        type = db.Column(db.String(2), nullable=False)
-        lash_type = db.Column(db.String(2), nullable=True)
-        notes = db.Column(db.Text, nullable=True)
-
-        def __repr__(self):
-            return f"<Lash {self.id}>"
-
-    class Brow(db.Model):
-        id = db.Column(db.Integer, primary_key=True)
-        client_id = db.Column(
-            db.Integer,
-            db.ForeignKey('client.id'),
-            nullable=False
-            )
-        type = db.Column(db.String(2), nullable=False)
-        brow_type = db.Column(db.String(2), nullable=True)
-        notes = db.Column(db.Text, nullable=True)
-
-        def __repr__(self):
-            return f"<Brow {self.id}>"
-
-    id = db.Column(db.Integer, primary_key=True)
-    type = db.Column(db.String(2), nullable=False)
-    notes = db.Column(db.Text, nullable=True)
-    lash_id = db.Column(db.Integer, db.ForeignKey('lash.id'), nullable=True)
-    lash = db.relationship("Lash", backref="treatment", uselist=False)
-    brow_id = db.Column(db.Integer, db.ForeignKey('brow.id'), nullable=True)
-    brow = db.relationship("Brow", backref="treatment", uselist=False)
-
-    def __repr__(self):
-        treatment_type = ""
-        if self.type == "L":
-            treatment_type = "Lashes"
-        elif self.type == "B":
-            treatment_type = "Brows"
-        elif self.type == "BL":
-            treatment_type = "Both Lashes and Brows"
-        return f"<Treatment {self.id} ({treatment_type})>"
