bcb3603b38e884f84997070fece6b2c14360d099
Nic <nicolecadman@icloud.com>
2023-05-15 22:44:03 +0000

    redundant code removed from file

--- treatments/static/js/script.js
+++ treatments/static/js/script.js
@@ -1,16 +1,9 @@
 document.addEventListener("DOMContentLoaded", function() {
-    // sidenav initialization
-    let sidenav = document.querySelectorAll(".sidenav");
-    M.Sidenav.init(sidenav);
-
     // select initialization
     let selects = document.querySelectorAll("select");
     M.FormSelect.init(selects);
 
-    // collapsible initializataion
-    let collapsibles = document.querySelectorAll(".collapsible");
-    M.Collapsible.init(collapsibles);
-
+    // dropdown initialization
     var elems = document.querySelectorAll('.modal');
     var instances = M.Modal.init(elems);
 });
